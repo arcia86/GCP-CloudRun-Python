d7915b2d77e2438b7b088c66ba5c412fc3e9238d
arcia86 <arcia86alfredo@gmail.com>
2022-10-18 16:27:41 -0300

    add print my header fix geo2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def hello_world():
     name = os.environ.get("NAME", "World")
     geo = request.headers.get('X-Client-Geo-Location')
     if geo == None:
-        geo == "No country"
+        geo = "No country"
         return "Hello {}!".format(name) + "I am come from =" + geo
     else:
         return "Hello {}!".format(name) + "I am come from =" + geo 
