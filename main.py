c347a5379f11144b48470cccbb79e2ed8ee29446
arcia86 <arcia86alfredo@gmail.com>
2022-10-18 14:52:24 -0300

    add print my header

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -8,8 +9,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+    geo = request.headers.get('X-Client-Geo-Location')
+    return "Hello {}!".format(name) + "I am come from =" + geo 
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("HTTP_PORT")))
+    """app.run(debug=True, host="127.0.0.1", port=9000)"""
